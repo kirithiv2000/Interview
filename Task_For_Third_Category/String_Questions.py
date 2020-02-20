abf97cfe591cf38f06ac15b15e3e374a9dca905d
Kirithiv R <52394696+kirithiv2000@users.noreply.github.com>
2020-02-20 17:59:21 +0530

    Update String_Questions.py

--- Task_For_Third_Category/String_Questions.py
+++ Task_For_Third_Category/String_Questions.py
@@ -1,5 +1,5 @@
 #1.String Questions
-    #WAP to print duplicate characters in a string.
+    #1.WAP to print duplicate characters in a string.
 
 def print_duplicate_charactersInString(str):   #def is a keyword to declare user defined function 
     que=""                                     #Declaring variable que with empty string
@@ -14,7 +14,18 @@ def print_duplicate_charactersInString(str):   #def is a keyword to declare user
 
 print_duplicate_charactersInString('kirithivvr') #calling the user defined function
 
-    #WAP to print the first non-repeated character in a string.
+    #2.WAP to check if two strings are anagrams of each other.
+def anagram_finder(str1, str2):                 #declaration of function       
+    list_str1 = list(str1)                      #conver the str in list
+    list_str2 = list(str2)                      #conver the str in list
+    if sorted(list_str1) == sorted(list_str2):  #check both list are equal   
+        return True                             #if both are same then return true
+    else:                                       #not same or not equal
+        return False                            #then return False
+
+print(anagram_finder('vihtirik','kirithiv'))    #calling the function
+
+    #3.WAP to print the first non-repeated character in a string.
 def firstNon_RepeatedCharacter(str):             #Name of user defined function is firstNon_RepeatedCharacter
     for character in range(len(str)):            #Iterating using for Loop   
         firstHalf=str[:character]                #part of string before character
@@ -25,3 +36,54 @@ def firstNon_RepeatedCharacter(str):             #Name of user defined function 
                 return str[character]            #returning the first non-repeated character
 
 firstNon_RepeatedCharacter('kirithiv')           #calling the function of name firstNon_RepeatedCharacter
+
+
+    #4.WAP to reverse a given string using recursion.
+def reverse_a_string(str):                      #declaration of function
+    if len(str) == 0:                           #if the length of string is 0
+        return (str)                            #return the string
+    else:                                       #if lenght is not 0
+        var=reverse_a_string(str[1:])           #function calls itself with string without first character
+        return (var + str[0])                   #function concatenation
+
+print(reverse_a_string('kirithiv'))             #calling the function
+
+
+    #5.WAP to check if a string contains only digits.
+def stringOnlyContainDigitsCheck(givenString):  #declaration of function
+    digits='0123456789'                         #declaring a variable digits with all the digits
+    for character in givenString:               #iterating over givenString
+        if character in digits:                 #check weather character is int or a alphabet
+            pass                                #passing to next line
+        else:                                   #if it is an alphabet
+            return False                        #return false
+    return True                                 #if the for loop run succesfully then the function will return True
+
+
+
+    #7.WAP to count the number of vowels and consonants in a given string.
+def countVowelConsonants(str):
+    vowels="aeiou"                               #declaring a variable named vowels with english vowels in it
+    space=0                                      #declare a variable with value 0
+    vowelsCount=0                                #declare a variable with value 0
+    for character in str.lower():                #iterating over string after converting string into lower case
+        if character in vowels:                  #check it is vowel 
+            vowelsCount+=1                       #incriment for count
+        if character == " ":                     #check it is space
+            space+=1                             #incriment for space
+    consonantsCount=len(str)-vowelsCount         #FROM the length if we seprate count of vowels and
+    consonantsCount-=space                       #space in the string we will get the no of consonants
+    return [vowelsCount,consonantsCount]         #return count of vowels and count of consonants in a list
+
+
+print(countVowelConsonants('kirithiv'))          #calling the function
+
+
+
+
+    #12.WAP to check if a given string is a palindrome.
+def palindrome(user1):                           #declaration of function      
+	return True if user1[::-1]==user1 else False #return true if palindrome else false with the help of slicing
+print(palindrome('nine'))                        #calling the function
+
+
